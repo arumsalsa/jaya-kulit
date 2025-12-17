e333454226bdcfa9ab928f1a2db64585f8d12fa1
arumsalsa <arumsalsabila027@gmail.com>
2025-12-17 11:22:04 +0700

    build vite assets

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -21,7 +21,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite(['../resources/css/app.css', '../resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-50 text-gray-800">
